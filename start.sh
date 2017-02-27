e099c11d1785bcc0b3875746e655def5c0eda414
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-02-27 08:32:51 +0000

    Update email for certbot

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-certbot certonly --standalone --non-interactive --agree-tos --email vitaliy@vitalcode.uk \
+certbot certonly --standalone --non-interactive --agree-tos --email vitaliy.kuznetsov@yahoo.co.uk \
     -w /var/www/fillyourday \
     -d fillyourday.com \
     -d www.fillyourday.com
